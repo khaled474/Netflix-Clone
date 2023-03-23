08f7fd89e0592586844138a55d8cf7ebd21f8267
khaled <khaled.abdelmohsen@icloud.com>
2023-03-23 18:49:58 +0200

    Initial Commit

---
+++ Netfilx AppUITests/Netfilx_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Netfilx_AppUITestsLaunchTests.swift
+//  Netfilx AppUITests
+//
+//  Created by Khaled abdelmohsen on 23/03/2023.
+//
+
+import XCTest
+
+final class Netfilx_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
